91a1d75240bd8893406523b153dd0a9c77a75249
Morix <vitalik2001vaskin@gmail.com>
2022-05-14 12:16:26 +0300

    Create cloud:texture, sprite

--- main.cpp
+++ main.cpp
@@ -28,13 +28,34 @@ int main()
     textureBee.loadFromFile("graphics/bee.png");
     sf::Sprite spriteBee;
     spriteBee.setTexture(textureBee);
-    spriteBee.setPosition(0, 800* scaleF);
+    spriteBee.setPosition(0, 800*scaleF);
     spriteBee.setScale(scaleF, scaleF);
 
     bool beeActive = false;
     double beeSpeed = 0.0;
 
     sf::Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+    sf::Sprite spriteCloud_1;
+    sf::Sprite spriteCloud_2;
+    sf::Sprite spriteCloud_3;
+    spriteCloud_1.setTexture(textureCloud);
+    spriteCloud_2.setTexture(textureCloud);
+    spriteCloud_3.setTexture(textureCloud);
+    spriteCloud_1.setPosition(0, 0);
+    spriteCloud_2.setPosition(0, 250 * scaleF);
+    spriteCloud_3.setPosition(0, 500 * scaleF);
+    spriteCloud_1.setScale(scaleF, scaleF);
+    spriteCloud_2.setScale(scaleF, scaleF);
+    spriteCloud_3.setScale(scaleF, scaleF);
+
+    bool cloud1_Active = false;
+    bool cloud2_Active = false;
+    bool cloud3_Active = false;
+
+    double cloud1_Speed = 0.0;
+    double cloud2_Speed = 0.0;
+    double cloud3_Speed = 0.0;
 
     while (window.isOpen())
     {
@@ -44,6 +65,11 @@ int main()
         }
         window.clear();
         window.draw(spriteBackground);
+
+        window.draw(spriteCloud_1);
+        window.draw(spriteCloud_2);
+        window.draw(spriteCloud_3);
+
         window.draw(spriteTree);
         window.draw(spriteBee);
         window.display();
